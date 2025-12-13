a6a57e8b36ecb2e53a3490223a16e1c99854ae8f
nihal@0101 <nihal@mapout.com>
2025-12-14 01:57:26 +0400

    Initial commit, basic server setup done

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'OK' });
+});
+
+export default app;
