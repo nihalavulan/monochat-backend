4d3f7b3b4d27b31d9362458cb2e26379f678b926
nihal@0101 <nihal@mapout.com>
2025-12-14 02:39:13 +0400

    mongodb setup done

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import http from 'http';
 import app from './app';
 import { setupSocket } from './socket';
+import dotenv from 'dotenv';
+import { connectDB } from './db';
+
+
+dotenv.config()
 
 const server = http.createServer(app);
 setupSocket(server);
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
